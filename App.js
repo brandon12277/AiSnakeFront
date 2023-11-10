839f60ced3e51adf34b0795131a568b35a527862
Brandon Williams <76258619+brandon12277@users.noreply.github.com>
2023-11-10 09:09:34 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './App.css';
+import SnakeGame from './snake';
+
+function App() {
+  return (
+    <div className="App">
+       <SnakeGame/>
+    </div>
+  );
+}
+
+export default App;
